b99a9d106dbb89873cbe0d2845af526561e1f06b
NicolasDev ccxc <nsabogal@ccxc.us>
2021-04-07 09:03:51 -0500

    try-one

---
+++ JsonsExamples/ThemeOne.js
@@ -0,0 +1,14 @@
+const themeOne = {
+    "Header" : {
+        title : {
+            contente: "Test Uno",
+            style:{
+                fontSize:24,
+                color: 'darkorchid',
+            }
+        },
+        style : {
+            height: '20px',
+        }
+    }
+}
